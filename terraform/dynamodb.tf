9233fcd0b120ca736e6b1b86378649446717ac5d
Bhanu <pachipala.b@northeastern.edu>
2026-02-28 12:42:13 -0500

    infra: add Kinesis data stream and DynamoDB tables

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,1 +1,72 @@
+resource "aws_dynamodb_table" "live_prices" {
+  name         = "${var.project_name}-live-prices"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "symbol"
+  range_key    = "timestamp"
 
+  attribute {
+    name = "symbol"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-live-prices"
+  }
+}
+
+resource "aws_dynamodb_table" "price_candles" {
+  name         = "${var.project_name}-candles"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "symbol"
+  range_key    = "candle_timestamp"
+
+  attribute {
+    name = "symbol"
+    type = "S"
+  }
+
+  attribute {
+    name = "candle_timestamp"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-candles"
+  }
+}
+
+resource "aws_dynamodb_table" "anomalies" {
+  name         = "${var.project_name}-anomalies"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "symbol"
+  range_key    = "detected_at"
+
+  attribute {
+    name = "symbol"
+    type = "S"
+  }
+
+  attribute {
+    name = "detected_at"
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.project_name}-anomalies"
+  }
+}
--- terraform/kinesis.tf
+++ terraform/kinesis.tf
@@ -1,1 +1,13 @@
+resource "aws_kinesis_stream" "main" {
+  name             = "${var.project_name}-stream"
+  shard_count      = 1
+  retention_period = 24
 
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+
+  tags = {
+    Name = "${var.project_name}-stream"
+  }
+}
